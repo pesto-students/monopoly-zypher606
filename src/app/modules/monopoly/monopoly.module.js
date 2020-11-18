e8a367d27808572ea06d56ae6de0f3c58d4e273e
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-11-18 16:53:19 +0530

    Fix router for gameplay view

--- src/app/modules/monopoly/monopoly.module.js
+++ src/app/modules/monopoly/monopoly.module.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import HomeComponent from "./components/home/home.component";
 import GameplayComponent from './components/gameplay/gameplay.component';
 
 function Monopoly(props) {
     return (
         <Router basename={props.match.path}>
+            <Redirect exact from="/" to="gameplay" />
             <Route exact path="/" component={ HomeComponent } />
             <Route exact path="/gameplay" component={ GameplayComponent } />
         </Router>
